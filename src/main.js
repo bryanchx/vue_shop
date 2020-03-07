39a02f2b3bf806d3cdc8d7199885d87caca00ab4
chx2379 <bryanchxm@gmail.com>
2020-03-07 23:43:41 +0800

    1.eslint配置.prettierrc规则 2.login逻辑优化 3.axios配置

--- src/main.js
+++ src/main.js
@@ -5,6 +5,11 @@ import store from './store'
 import './plugins/element.js'
 import './assets/global.css'
 import './assets/fonts/iconfont.css'
+import axios from 'axios'
+
+Vue.prototype.$http = axios
+//配置请求的根路径
+axios.defaults.baseURL='http://127.0.0.1:8888/api/private/v1'
 
 Vue.config.productionTip = false
 
